ea0a10a415e6b13eba5ae82a4774d58d4327c089
Vivek-Prajapatii <prajapativivek415@gmail.com>
2023-12-15 20:18:14 +0530

    made responsive

--- src/components/header/SignupBar.tsx
+++ src/components/header/SignupBar.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import "../../styles/components/header/SignupBar.scss"
+import "../../styles/components/header/SignupBar.scss";
+import MenuIcon from "@mui/icons-material/Menu";
 
 function SignupBar() {
   return (
     <div className="signup-bar-container">
-      <div><a href="">Subscribe</a></div>
+      <div className="subscribe">
+        <MenuIcon className="menu-icon"/>
+        <a>Subscribe</a>
+      </div>
       <h1>Large</h1>
       <div className="search-bar-and-signup">
         <div className="search"></div>
